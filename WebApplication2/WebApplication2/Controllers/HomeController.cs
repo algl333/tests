beff73acb30170489e0aa13fe1815a372150d225
unknown <429193-6>
2024-04-03 16:16:38 +0300

    алиса

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Title = 'MyCafe';
+            return View();
+        }
+        public ViewResult DrinkForm()
+        {
+            ViewBag.Title = 'Drinks';
+            return View();
+        }
+    [HttpPost]
+    public ViewResult DrinkForm(DrinkData drinkData)
+        { 
+          if (ModelState.IsValid)
+        {
+            ViewBag.Title = "Bill";
+            //Определить стоимость напитка и вернуть в ViewBag.Cost 
+            ViewBag.Cost = 150;
+            return View("Bill", drinkData);
+        } 
+    else 
+    {
+    return View();
+    }
+    }
+
+}
---
+++ WebApplication2/WebApplication2/Models/DrinkData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class DrinkData
+    {
+        public DrinkType DrinkType { get; set; } 
+        [Required(ErrorMessage = 'Пожалуйста, введите молоко')]
+        public int Milk{get; set;}
+        [Required(ErrorMessage = 'Пожалуйста, введите сахар')]
+        public int Sugar { get; set; }
+
+    }
+    public enum DrinkType
+    {
+        Tea, 
+        Coffee,
+        Juice,
+        Alcohol
+    }
+}
